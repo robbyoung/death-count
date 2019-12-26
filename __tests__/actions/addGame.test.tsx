aae4bd3aab70902e0b9d249b22a554bc9b003b4d
Robert Young <robertyoung10101@gmail.com>
2019-12-27 09:37:27 +1300

    Wrote tests for new game screen

--- __tests__/actions/addGame.test.tsx
+++ __tests__/actions/addGame.test.tsx
@@ -1,5 +1,6 @@
 import { addGameAction } from '../../app/actions';
 import gamesReducer from '../../app/reducers/games';
+import { createTestState } from '../../app/state';
 
 describe('Add Game Action', () => {
     it('can add a death to an empty list', () => {
@@ -8,5 +9,13 @@ describe('Add Game Action', () => {
         const newState = gamesReducer(state, action);
         expect(newState).toEqual([action.newGame]);
         expect(state).toEqual([]);
+    });
+
+    it('will mark the new game as selected', () => {
+        const action = addGameAction('test action');
+        const state = createTestState(2, 2, 0, 1, 1).games;
+        const newState = gamesReducer(state, action);
+        expect(newState).toEqual([...createTestState(2, 2, 0, -1, 1).games, action.newGame]);
+        expect(state).toEqual(createTestState(2, 2, 0, 1, 1).games);
     });
 })
---
+++ __tests__/containers/newGame.test.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import store from '../../app/store';
+import { addGameAction, addDeathAction, addOptionSetAction } from '../../app/actions';
+import NewGame from '../../app/containers/newGame';
+
+const fakeNavigation = {
+    goBack: () => undefined,
+}
+
+describe('New Game Container', () => {
+    it('Renders without any option sets', async () => {
+        store.dispatch(addGameAction('Test Game'));
+        
+        const component = renderer.create(<NewGame navigation={fakeNavigation as any} />);
+        expect(component.toJSON()).toMatchSnapshot();
+    });
+
+    it('Renders with option sets', async () => {
+        const gameAction = addGameAction('Test Game');
+        store.dispatch(gameAction);
+        store.dispatch(addOptionSetAction('Set 1', gameAction.newGame.id));
+        store.dispatch(addOptionSetAction('Set 2', gameAction.newGame.id));
+        
+        const component = renderer.create(<NewGame navigation={fakeNavigation as any} />);
+        expect(component.toJSON()).toMatchSnapshot();
+    });
+});
