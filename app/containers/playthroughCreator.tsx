d0d0d484d1fc58e16bbdb7a1aff30ff03ef70474
Robert Young <robertyoung10101@gmail.com>
2020-02-18 19:04:07 +1300

    Hooked a game picker up to the playthrough creator

--- app/containers/playthroughCreator.tsx
+++ app/containers/playthroughCreator.tsx
@@ -3,6 +3,10 @@ import React, { Component } from 'react';
 import { NavigationInjectedProps } from 'react-navigation';
 import { backgroundColor, white, buttonColor } from '../colors';
 import Input from '../components/input';
+import OptionPicker from '../components/optionPicker';
+import { Game, Playthrough } from '../state';
+import store from '../store';
+import { getAllGames, getIncompletePlaythrough } from '../selectors';
 
 const styles = StyleSheet.create({
     header: {
@@ -14,7 +18,13 @@ const styles = StyleSheet.create({
     }
 });
 
-export default class PlaythroughCreator extends Component<NavigationInjectedProps> {
+interface PlaythroughCreatorState {
+    games: Game[];
+    playthrough: Playthrough;
+}
+export default class PlaythroughCreator extends Component<NavigationInjectedProps, PlaythroughCreatorState> {
+    private unsubscribe = () => undefined;
+
     public static navigationOptions = () => {
         return {
             title: 'New Playthrough',
@@ -23,18 +33,55 @@ export default class PlaythroughCreator extends Component<NavigationInjectedProp
         };
     };
 
+    public componentDidMount() {
+        this.refreshState();
+        this.unsubscribe = store.subscribe(() => this.refreshState());
+    }
+
+    public componentWillUnmount() {
+        this.unsubscribe();
+    }
+
     public render() {
+        if (!this.state) {
+            return (
+                <View/>
+            );
+        }
+
         return (
             <View style={styles.background}>
                 <Input
                     title="Playthrough Name"
                     onTextChange={text => this.onNameUpdate(text)}>
                 </Input>
+
+                <OptionPicker
+                    options={this.state.games.map(game => ({
+                        key: game.name,
+                        value: game.id,
+                    }))}
+                    onSelect={key => this.onGameUpdate(key)}
+                    selected={this.state.playthrough.gameId}>
+
+                </OptionPicker>
             </View>
         );
     }
 
+    private refreshState() {
+        const state = store.getState();
+        this.setState({
+            games: getAllGames(state),
+            playthrough: getIncompletePlaythrough(state),
+        })
+    }
+
     private onNameUpdate(text: string) {
+
+    }
+
+    private onGameUpdate(gameId: string) {
 
     }
 }
